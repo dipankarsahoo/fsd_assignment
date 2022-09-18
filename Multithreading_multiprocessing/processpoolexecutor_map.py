5a5e46ff9d1d70053ea6275808ff7f09dd8e988a
Dipankar.Sahoo <Dipankar.Sahoo@bottomline.com>
2022-09-18 19:05:09 +0530

    process pool executor

---
+++ Multithreading_multiprocessing/processpoolexecutor_map.py
@@ -0,0 +1,20 @@
+import concurrent.futures
+import time
+
+start = time.perf_counter() 
+
+def do_something(seconds): 
+    print(f'Sleeping for {seconds} now')
+    time.sleep(seconds)
+    print(f'Done sleeeping for {seconds}')
+    
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    secs = [5,4,3,2,1]
+    results = executor.map(do_something, secs)
+    
+    for result in results:  
+        print(result)
+    
+finish = time.perf_counter()
+
+print(f'Sleeping for {round(finish-start, 2)} seconds')
